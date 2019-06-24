bb32b261ab88c72d777f821334c3f95a11fb5d36
Jason Mavandi <mvndaai@gmail.com>
2019-06-23 22:45:50 -0600

    start making it configurable

--- js/chapel_svg.js
+++ js/chapel_svg.js
@@ -7,17 +7,14 @@ const aisle = personSize * 2;
 
 
 const viewbox = {
-    x: 2000, y: 2000,
-
-    // x: borderSpace * 2 + aisle * 2 + w.side * 2 + w.center,
-    // y: borderSpace * 2 + aisle * 2 + h.stand + h.congregation + h.overflow,
+    x: 500, y: 400,
 };
 
 const selectorSkew = 10;
 const showSelectors = false;
 
 /** rowThreeSections
- * top, rows, sections: {seats, passers}
+ * top, rows, seats: [#...]
 */
 const rowSections = {
     view: vnode => [
@@ -27,7 +24,7 @@ const rowSections = {
 
             for (let j = i -1; j >= 0; j--){
                 let prev = vnode.attrs.sections[j]
-                left += aisle + prev.seats * personSize
+                left += aisle + prev * personSize
             }
 
             return [
@@ -36,7 +33,7 @@ const rowSections = {
                         y: vnode.attrs.top,
                         x: left,
                         height: vnode.attrs.rows * personSize,
-                        width: section.seats * personSize,
+                        width: section * personSize,
                     }
                 ),
                 showSelectors && [
@@ -46,7 +43,7 @@ const rowSections = {
                     }, 'a'),
                     m('text.selector', {
                         y: vnode.attrs.top + personSize,
-                        x: left + selectorSkew + section.seats * personSize,
+                        x: left + selectorSkew + section * personSize,
                     }, 'b'),
                     m('text.selector', {
                         y: vnode.attrs.top + vnode.attrs.rows * personSize,
@@ -54,7 +51,7 @@ const rowSections = {
                     }, 'c'),
                     m('text.selector', {
                         y: vnode.attrs.top + vnode.attrs.rows * personSize,
-                        x: left + selectorSkew + section.seats * personSize,
+                        x: left + selectorSkew + section * personSize,
                     }, 'd'),
                 ],
             ];
@@ -62,35 +59,65 @@ const rowSections = {
     ]
 };
 
+const sectionsKey = 'sections';
+const passersKey = 'passers';
+const rowsKey = 'rows';
+const seatsKey = 'seats';
+const displayedSectionsKey = 'displayedSections';
 
-let temp =  [
-    {
-        rows: 2,
-        sections: [
-            {seats: 4},
-            {seats: 10},
-            {seats: 4},
-        ],
-    },
-    {
-        rows: 8,
-        sections: [
-            {seats: 4},
-            {seats: 10},
-            {seats: 4},
-        ],
-    },
-    {
-        rows: 4,
-        sections: [
-            {seats: 9},
-            {seats: 9},
-        ],
-    },
-];
+function initDisplayedSection(rows, seats) {
+    const ds = {
+        [rowsKey]: 2,
+        [seatsKey]: [4, 10, 4],
+    };
+    if (rows) ds[rowsKey] = rows;
+    if (seats) ds[seatsKey] = seats
+    return ds;
+}
+
+const configValues = {
+    [passersKey]: 8,
+    [sectionsKey]: [
+        initDisplayedSection(2, [4, 10, 4]),
+        initDisplayedSection(8 , [4, 10 ,4]),
+        initDisplayedSection(4, [9, 9]),
+    ],
+    [displayedSectionsKey]: [
+        initDisplayedSection(2, [4, 10, 4]),
+        initDisplayedSection(8 , [4, 10 ,4]),
+        initDisplayedSection(4, [9, 9]),
+    ],
+};
+
+const Configuration = {
+    view: _ => m('',
+        m('.input',
+            m('label', {for: passersKey}, 'Passers: '),
+            m('input', {id: passersKey, type: 'number', min: '1', max: '16',
+                value: configValues[passersKey],
+                onchange: e => configValues[passersKey] = e.target.value,
+            }),
+        ),
+        m('.input',
+            m('label', {for: sectionsKey}, 'Sections: '),
+            m('input', {id: sectionsKey, type: 'number', min: '2', max: '3',
+                value: configValues[displayedSectionsKey].length,
+                onchange: e => {
+                    const out = [];
+                    for(var i=0; i < e.target.value; i++){
+                        if (configValues[sectionsKey].length +1 < i){
+                            configValues[sectionsKey].push(initDisplayedSection());
+                        }
+                        out.push(configValues[sectionsKey][i]);
+                    }
+                    configValues[displayedSectionsKey] = out;
+                },
+            }),
+        ),
+    )
+};
 
 
-const passers = 8;
 // TODO figure this out
 const passX = 364;
 const passY = 120;
@@ -99,31 +126,38 @@ const passY = 120;
 const ChapelSvg = {
     view: vnode => m('svg', {width: viewbox.x, height: viewbox.y, viewBox: `0 0 ${viewbox.x} ${viewbox.y}` },
 
-       temp.map((row, i) => {
+    configValues[displayedSectionsKey].map((row, i) => {
             console.log(i, row);
 
             let top = personSize
             for (let j = i -1; j >= 0; j--){
-                let prev = temp[j]
+                let prev = configValues[displayedSectionsKey][j]
                 top += aisle + prev.rows * personSize
             }
 
             return m(rowSections, {
                 top: top,
-                rows: row.rows,
-                sections: row.sections,
+                rows: row[rowsKey],
+                sections: row[seatsKey],
             })
         }),
 
 
-        Array(passers).fill(0).map((_, i) =>
+        Array(configValues[passersKey]).fill(0).map((_, i) =>
             m(`text.passer-${i}`, {
-                x: passX + personSize + personSize * (i % (passers/2)),
-                y: passY + personSize * Math.floor(i / (passers/2)),
+                x: passX + personSize + personSize * (i % (configValues[passersKey]/2)),
+                y: passY + personSize * Math.floor(i / (configValues[passersKey]/2)),
                 opacity:0.5,
             }, i + 1)
         ),
     )
 };
 
-export default ChapelSvg;
+const Arrangment = {
+    view: _ => m('.flex',
+        m(Configuration),
+        m(ChapelSvg),
+    )
+};
+
+export default Arrangment;
